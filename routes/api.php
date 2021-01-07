74d62a3ccfa3d26d684ea0e2836d773b2db8598f
vikasyuvasoft <vikasyuvasoft294@gmail.com>
2021-01-07 11:41:15 +0530

    change again api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('/welcome2', function () {
-    return view('welcome');
-});   
